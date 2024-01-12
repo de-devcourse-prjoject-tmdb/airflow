e3bacd68e0c8e2e4f16c64b791b1292d6dd92006
eastnine90 <eastning90@gmail.com>
2024-01-12 22:35:45 +0900

    test용 DAG 수정(clound function call -> external 테이블 생성 -> merge)

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -1,8 +1,13 @@
+import json
 from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.providers.google.cloud.operators.bigquery import BigQueryExecuteQueryOperator
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryExecuteQueryOperator, \
+    BigQueryCreateExternalTableOperator
 
-# Define default_args dictionary to specify the default parameters for the DAG
+# fixed date value for test purpose.
+FIXED_DATE = '2024-01-12'
+
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -13,27 +18,48 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-delete_sql = '''
+delete_sql = f'''
 DELETE FROM `tmdb-movies-410603.dev.movie_details` d
 WHERE d.id IN (
     SELECT id
-      FROM `tmdb-movies-410603.dev.changes-2024-01-11`
+      FROM `tmdb-movies-410603.dev.changes-{FIXED_DATE}`
 )
 '''
-merge_sql = '''
+merge_sql = f'''
 MERGE `tmdb-movies-410603.dev.movie_details` d
-USING `tmdb-movies-410603.dev.changes-2024-01-11` c
+USING `tmdb-movies-410603.dev.changes-{FIXED_DATE}` c
 ON d.id = c.id
 WHEN NOT MATCHED THEN
   INSERT ROW
 '''
 
 with DAG(
-    'bigquery_merge_table_test',
-    default_args=default_args,
-    description='A simple DAG to create a new table in BigQuery from an existing table',
-    schedule_interval=None
+        'bigquery_merge_table_test',
+        default_args=default_args,
+        description='A simple DAG to create a new table in BigQuery from an existing table',
+        schedule_interval=None
 ) as dag:
+    fetch_changes = SimpleHttpOperator(
+        task_id='merge_test_function',
+        method='POST',
+        http_conn_id='functions_http',
+        endpoint='tmdb-changes',
+        headers={"Content-Type": "application/json"},
+        data=json.dumps({'date': f'{FIXED_DATE}'}),
+        extra_options={'timeout': 3600},
+        dag=dag,
+    )
+
+    create_external_table = BigQueryCreateExternalTableOperator(
+        task_id='merge_test_create',
+        bucket='tmdb-movies-dl',
+        source_objects=[f'changes-{FIXED_DATE}.json', ],
+        destination_project_dataset_table=f'tmdb-movies-410603.dev.changes-{FIXED_DATE}',
+        source_format='NEWLINE_DELIMITED_JSON',
+        schema_object='details_schema.json',
+        dag=dag,
+    )
+
     delete_existing_rows = BigQueryExecuteQueryOperator(
         task_id='merge_test_delete',
         sql=merge_sql,
@@ -50,4 +76,4 @@ with DAG(
         dag=dag,
     )
 
-delete_existing_rows >> merge_changes
+fetch_changes >> create_external_table >> delete_existing_rows >> merge_changes
